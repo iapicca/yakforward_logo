e48cec7a1e361dc9c70fd31d6984a066bb1f50ed
yakforward <yakforward@gmail.com>
2022-06-20 17:26:11 +0300

    fix wrong asset

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:yakforward_logo/yakforward_logo.dart';
+import 'dart:typed_data';
 
 void main() => runApp(const MaterialApp(home: Home()));
 
@@ -7,7 +9,24 @@ class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(context) => const Material(
-        child: Center(child: Icon(YakfForwardLogo.iconData)),
+  Widget build(context) => Material(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Center(child: Icon(YakfForwardLogo.iconData)),
+            Center(child: LogoSvg()),
+          ],
+        ),
+      );
+}
+
+class LogoSvg extends StatelessWidget {
+  const LogoSvg({super.key});
+
+  @override
+  Widget build(context) => FutureBuilder<Uint8List>(
+        builder: (context, svg) => !svg.hasData || svg.hasError
+            ? const SizedBox.shrink()
+            : SvgPicture.memory(svg.data!),
       );
 }
