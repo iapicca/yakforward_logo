30391ed7b73ebadaa20b23f218b41b963ed414c9
yakforward <yakforward@gmail.com>
2022-06-20 17:57:38 +0300

    roll back non IconData implementations

--- lib/yakforward_logo.dart
+++ lib/yakforward_logo.dart
@@ -1,10 +1,7 @@
 library yakforward_logo;
 
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
-const _svgAsset = '/assets/svg/yakforward-logo.svg';
 const _fontFamily = 'yakforward';
 const _codepoint = 0xe800;
 
@@ -16,19 +13,5 @@ abstract class YakforwardLogo {
   // build(context) => Icon(YakforwardLogo.iconData);
   // ```
   // */
-  static const iconData = IconData(_codepoint, fontFamily: _fontFamily);
-
-  /// `Uint8List` for `yakforward oü` logo svg
-  //**
-  // ```dart
-  // build(context) => FutureBuilder<>(
-  //   future: YakforwardLogo.svgData,
-  //   builder: (context,svg) => {{handle stuff}} SvgPicture.memory(svg.data!)
-  // );
-  // ```
-  // */
-  static Future<Uint8List> get svgData async {
-    final bytes = await rootBundle.load(_svgAsset);
-    return bytes.buffer.asUint8List();
-  }
+  static const icon = IconData(_codepoint, fontFamily: _fontFamily);
 }
